1ab3efc72d16bcda950cb5811c4f03471365edd2
theaxeisbest <axelwas07@gmail.com>
2021-05-09 10:30:40 +0200

    added first working prototype

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const $ = (query) => document.querySelector(query);
 
 const sphere = $('a-sphere');
 const plane = $('a-plane');
-
+/*
 const shiftDegrees = (value) => (value + 1) % 360;
 
 let degrees = 0;
@@ -28,3 +28,29 @@ const animate = () => {
 };
 
 requestAnimationFrame(animate);
+*/
+AFRAME.registerComponent('my-animation', { 
+ init: function () {
+   var self = this;
+   this.time = 0;
+   this.animation = AFRAME.ANIME({
+    targets: [{x: -Math.PI / 2, y: 0, z: 0}],
+    x: -Math.PI / 2, y: 0, z: 2*Math.PI,
+    autoplay: false,
+    duration: 20000,
+    easing: "linear",
+    loop: true,
+    round: false,
+    update: function (animation) {
+      var value = animation.animatables[0].target;
+      self.el.object3D.rotation.set(value.x, value.y, value.z);
+    }
+   });
+   this.animation.began = true;
+  
+ },
+ tick: function (t, dt) {
+   this.time += dt;
+   this.animation.tick(this.time);
+ }
+});
