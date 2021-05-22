be8f09af44f027c923e9dc6dcfce298f6b302495
theaxeisbest <axelwas07@gmail.com>
2021-05-22 13:12:28 +0200

    added rotation

--- index.js
+++ index.js
@@ -22,8 +22,13 @@ const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
 function animate() {
+  requestAnimationFrame(animate); 
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
   renderer.render(scene, camera);
-  requestAnimationFrame(animate);
 }
 
 animate()
